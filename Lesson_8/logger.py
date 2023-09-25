0424e95b7fd43835d96744267568738370329b75
Amruddy <embaev94@gmail.com>
2023-09-25 09:01:00 +0300

    add hw

---
+++ Lesson_8/logger.py
@@ -0,0 +1,28 @@
+from data_create import *
+
+def search_line():
+    print('Выбертите вариант поиска:\n'
+          '1. Имя\n'
+          '2. Фамилия\n'
+          '3. Отчество\n'
+          '4. Телефон\n'
+          '5. Адрес')
+    index = int(input('Введите вариант: ')) - 1
+    searched = input('Введите поисковые данные: ').title()
+    with open('book.txt', 'r', encoding='utf-8') as file:
+        data = file.read().strip().split('\n\n')
+        for item in data:
+            new_item = item.replace('\n', ' ').split()
+            if searched in new_item[index]:
+                print(item, end="\n\n")
+        # file.seek(0)
+        # print(file.readlines())
+
+def enter_data():
+    name = enter_first_name()
+    surname = enter_second_name()
+    family = enter_family_name()
+    number = enter_phone_number()
+    address = enter_address_number()
+    with open('book.txt', 'a', encoding='utf-8') as file:
+        file.write(f'{name} {surname} {family}\n{number}\n{address}\n\n')
